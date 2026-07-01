7cef2d9a7eeb6b7d61fac5cbae69fb58f5692c73
chaeb0414-collab <chaeb0414@gmail.com>
2026-07-01 10:42:00 +0900

    설계

---
+++ src/main/java/com/example/schedule/dto/CreateScheduleRequest.java
@@ -0,0 +1,10 @@
+package com.example.schedule.dto;
+
+import java.time.LocalDateTime;
+
+public class CreateScheduleRequest {
+    private String title;
+    private String content;
+    private String writer;
+    private String password;
+}
---
+++ src/main/java/com/example/schedule/dto/ScheduleResponse.java
@@ -0,0 +1,13 @@
+package com.example.schedule.dto;
+
+import java.time.LocalDateTime;
+
+public class ScheduleResponse {
+    private Long id;
+    private String title;
+    private String content;
+    private String writer;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
